36c4c8bffe1bba82f15d25579d0aa028f30569f3
OLHA55 <bagira2310@gmail.com>
2024-02-18 23:11:10 +0200

    js04new002

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,16 @@
-function isEnoughCapacity(products, containerSize) {
-    
+function isEnoughCapacity(products, containerSize) { 
+  let sum = 0;
+    const values = Object.values(products);
+    for (const item of values) {
+         sum += item;
+     }
+     if (sum <= containerSize) {
+         return true;
+     } else {
+         return false;
+     }
 }
-
+ 
 
 
 isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8); 
@@ -12,7 +21,8 @@ isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7);
 
 
 
-console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-console.log( isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+ console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));// true
+ console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));// false
+ console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));// true
+ console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); //false
+
